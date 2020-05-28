1801cadff63b9c7fee500a32841983786fcc2743
Marco Peluso <marco@pelu.so>
2020-05-28 08:59:43 +0200

    Modify zsh aliases.

--- modules/zsh/payload/.zsh/zshaliases.zsh
+++ modules/zsh/payload/.zsh/zshaliases.zsh
@@ -10,12 +10,10 @@
 # Source it via .zshrc or another zsh startup file.
 # ------------------------------------------------------------------------------
 
-
 # zsh
 # ------------------------------------------------------------------------------
 # source .zshrc
 alias szshrc='source ~/.zsh/.zshrc'
-
 
 # ls
 # ------------------------------------------------------------------------------
@@ -31,8 +29,16 @@ alias lh='ls -lhdF .*'
 # list only directories in long mode
 alias ld='ls -lhdF *(/)'
 
-
 # cd
 # ------------------------------------------------------------------------------
 alias cdn='cd ~/nextcloud'
 alias cddf='cd ~/nextcloud/dotfiles'
+alias cddevlab='cd ~/nextcloud/dev/lab'
+alias cdmobappex='cd ~/nextcloud/fra-uas/Module/current/m22-mobile-application-exercises-2001-liebehenschel-data-visualization/git/mobappex'
+alias cdmobdev1='cd ~/nextcloud-unisharing/mob-devices/00-projects/01-project-1/marco'
+alias cdmobdev2='cd ~/nextcloud-unisharing/mob-devices/00-projects/02-project-2/marco'
+alias cdswed='cd ~/nextcloud-unisharing/swe-d/03-collaboration/00-git'
+
+# other
+# ------------------------------------------------------------------------------
+alias code='/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code'
