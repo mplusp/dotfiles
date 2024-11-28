3d8674b208fc2cc1d4d00ea34a277b1a3c5a04b2
Marco Peluso <marco@pelu.so>
2024-11-28 20:10:45 +0100

    (wezterm): Disable fancy tab bar and make window background slightly transparent

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -20,9 +20,12 @@ end
 -- Appearance
 config.font_size = 14.0
 config.color_scheme = color_scheme_for_appearance(wezterm.gui.get_appearance())
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 10
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.native_macos_fullscreen_mode = false
+config.use_fancy_tab_bar = false
 
 -- Keybindings
 config.keys = {
